15cfaafe9dfc406a808a86b28df2c48021363dd9
unknown <monielouis31@gmail.com>
2025-03-27 19:58:02 +0100

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import '../styles/index.scss';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div id='accueil'>
+      <div className="container">
+        <img src="./images/hero.png" alt="" />
+        <img src="./images/bg-noise.png" alt="" />
+        <div className="content">
+          <h1 className="title">
+            Axion Consulting
+          </h1>
+          <p className="subtitle">
+            Transformez votre business avec le digital.
+          </p>
+          <a href="#" className='primary-btn'> Réserver un appel</a>
+        </div>
+        <p className="tag">
+          Découvrir nos services
+          <span></span>
+        </p>
+      </div>
+    </div>
   )
 }
 
